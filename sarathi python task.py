782ed8278c188ce68ab46d415be1397a6b43a5ec
Vijayasarathi06 <141705162+Vijayasarathi06@users.noreply.github.com>
2023-08-23 12:33:44 +0530

    looping Task

---
+++ sarathi python task.py
@@ -0,0 +1,43 @@
+##Task 1
+##stop=int(input("enter the stop value"))
+##for i in range(1,stop):
+##    print(i)
+##Task 2
+##stop=int(input("enter the stop value"))
+##for i in range(2,stop,2):
+##    print(i)
+##Task 3
+##stop=int(input("enter the stop value"))
+##for i in range(1,stop,2):
+##    print(i)
+##Task 4
+##stop=int(input("enter the stop value"))
+##name=str(input("enter the name"))
+##for i in range(1,stop):
+##    if(i%5==0):
+##        print(name)
+##    else:
+##        print(i)
+##Task 5
+##stop=int(input("enter the stop value"))
+##table=int(input("enter the tables"))
+##for i in range(1,stop):
+##    print(i,"x",table,"=",i*table)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
